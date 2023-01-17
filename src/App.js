21063b357688f19bc8db9945e4d719ef2ef10f21
Fatih Arapoglu <f3tt4n@gmail.com>
2023-01-17 18:57:20 +0300

    add visual skeleton and squares

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/m-header";
+import Footer from "./components/m-footer";
+import Main from "./components/m-main";
 
 const App = () => {
     return (
         <>
             <Header />
+            <Main />
+            <Footer />
         </>
     );
 };
---
+++ src/components/m-main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="main-card">1</div>
+            <div className="main-card">2</div>
+            <div className="main-card">3</div>
+            <div className="main-card">4</div>
+            <div className="main-card">5</div>
+            <div className="main-card">6</div>
+            <div className="main-card">7</div>
+            <div className="main-card">8</div>
+            <div className="main-card">9</div>
+            <div className="main-card">10</div>
+        </div>
+    );
+};
+
+export default Main;
